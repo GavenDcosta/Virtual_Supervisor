21ef80a959f1aa18da0c6f6c6fc7c3d6531186d4
Gaven Dcosta <112816730+TeddyGaven@users.noreply.github.com>
2023-03-11 10:24:58 +0530

    Add files via upload

--- Extension/Focus.js
+++ Extension/Focus.js
@@ -1,9 +1,9 @@
 
-const generateHTML = (pagename) => {
+const generateHTML = () => {
     return`
     <center><h1 class="error">404</h1></center>
     <hr>
-    <div class="main"><h1>Our Systems Have Detected this as a Harmful site</h1></div>
+    <div class="main"><h1>Virtual-Supervisor has detected that this is a harmful website</h1></div>
     <br><br>`
 };
 
@@ -51,35 +51,72 @@ const generateSTYLING = () => {
 // }
 
                                                                             
-switch(window.location.hostname){
-    case "www.snapchat.com":
-        document.head.innerHTML = generateSTYLING();
-        document.body.innerHTML = generateHTML("SHAPCHAT")
-        break;
+// switch(window.location.hostname){
+//     case "www.snapchat.com":
+//         document.head.innerHTML = generateSTYLING();
+//         document.body.innerHTML = generateHTML("SHAPCHAT")
+//         break;
 
-    case "www.facebook.com":
-        document.head.innerHTML = generateSTYLING();
-        document.body.innerHTML = generateHTML("FACEBOOK")
-        break;
+//     case "www.facebook.com":
+//         document.head.innerHTML = generateSTYLING();
+//         document.body.innerHTML = generateHTML("FACEBOOK")
+//         break;
 
-    case "www.instagram.com":
-        document.head.innerHTML = generateSTYLING();
-        document.body.innerHTML = generateHTML("INSTAGRAM")
-        break;
+//     case "www.instagram.com":
+//         document.head.innerHTML = generateSTYLING();
+//         document.body.innerHTML = generateHTML("INSTAGRAM")
+//         break;
 
-    case "www.netflix.com":
-        document.head.innerHTML = generateSTYLING();
-        document.body.innerHTML = generateHTML("NETFLIX")
-        break;
+//     case "www.netflix.com":
+//         document.head.innerHTML = generateSTYLING();
+//         document.body.innerHTML = generateHTML("NETFLIX")
+//         break;
 
-    case "www.twitter.com":
-        document.head.innerHTML = generateSTYLING();
-        document.body.innerHTML = generateHTML("TWITTER")
-        break;
+//     case "www.twitter.com":
+//         document.head.innerHTML = generateSTYLING();
+//         document.body.innerHTML = generateHTML("TWITTER")
+//         break;
 
-    case "www.reddit.com":
-        document.head.innerHTML = generateSTYLING();
-        document.body.innerHTML = generateHTML("REDDIT")
-        break;
-}
+//     case "www.reddit.com":
+//         document.head.innerHTML = generateSTYLING();
+//         document.body.innerHTML = generateHTML("REDDIT")
+//         break;
+// }
+const data = fetch("http://localhost:4000/api/Virtual-Supervisor")
+  .then((response) => response.json())
+  .then((data) => {
+    for(let i=0;i<data.length;i++){
+        if(window.location.hostname === data[i].url){
+            document.head.innerHTML = generateSTYLING()
+            document.body.innerHTML = generateHTML()
+
+            fetch('https://api.ipify.org/?format=json').then(results => results.json())
+            .then((data) => {
+              fetch('https://api.emailjs.com/api/v1.0/email/send', {
+                method: 'POST',
+                body: JSON.stringify({
+                service_id: 'service_twg08ib',
+                template_id: 'template_lj6fb9f',
+                user_id: 'XovYt7O1DRpHPkYaV',
+                template_params: {
+                  from_name: 'Virtual-Supervisor',
+                  email_id: 'virtualsupervisorminiproject@gmail.com',
+                  ip: `ip Address is ${data.ip}`,
+                  message: `we have detected ${window.location.hostname} as harmful`
+                }}),
+                headers: {
+                  "Content-Type" : "application/json"
+                }
+              }).then(function() {
+                  alert('Your mail is sent!');
+              }).catch(function(error) {
+              alert('Oops... ' + JSON.stringify(error));
+              });
+            })
+    
+        }
+    }
+  })
+//   .then((data) => console.log(data));
+
 
